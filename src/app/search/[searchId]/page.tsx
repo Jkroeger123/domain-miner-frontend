d34b4357200fa306947c8e4f507ebb167e9c7884
Justin Kroeger <jkroeger@atlassian.com>
2024-08-13 13:24:40 -0400

    show prompt

--- src/app/search/[searchId]/page.tsx
+++ src/app/search/[searchId]/page.tsx
@@ -1,11 +1,19 @@
 import DomainPoller from "@/components/domain-poller";
 import { getDomains } from "@/server/domains";
+import { getSearch } from "@/server/search";
+import { notFound } from "next/navigation";
 
 export default async function SearchPage({
   params,
 }: {
   params: { searchId: string };
 }) {
+  const search = await getSearch(params.searchId);
+
+  if (!search) {
+    return notFound();
+  }
+
   const {
     domains: initialDomains,
     searching,
@@ -19,6 +27,7 @@ export default async function SearchPage({
   return (
     <div className="container mx-auto py-8">
       <h1 className="mb-4 text-2xl font-bold">Search Results</h1>
+      <p className="italic">{`"${search.prompt}"`}</p>
       <DomainPoller
         searchId={params.searchId}
         initialSearching={searching}
